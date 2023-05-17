b761185cb8055321b6e26176664ef7e90a6e8e67
Sadid <sadid.jones@gmail.com>
2023-05-17 23:19:44 +0600

    9 - Radio Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import "./App.css";
-import { MuiTextField } from "./components/MuiTextField";
+// import { MuiTextField } from "./components/MuiTextField";
 // import { MuiButton } from "./components/MuiButton";
 // import { MuiTypography } from "./components/MuiTypography";
+import { MuiSelect } from "./components/MuiSelect";
 
 function App() {
   return (
     <div className="App">
       {/* <MuiTypography></MuiTypography> */}
       {/* <MuiButton /> */}
-      <MuiTextField ></MuiTextField>
+      {/* <MuiTextField></MuiTextField> */}
+      <MuiSelect></MuiSelect>
     </div>
   );
 }
